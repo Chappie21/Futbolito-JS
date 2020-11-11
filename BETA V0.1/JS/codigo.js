7bb6c49b7b5ce455663fc173bd6e2d6a824e39b4
Andrés Chaparro <66017759+Chappie21@users.noreply.github.com>
2020-11-11 17:07:49 -0400

    Add files via upload

--- BETA V0.1/JS/codigo.js
+++ BETA V0.1/JS/codigo.js
@@ -3,11 +3,12 @@
 //import {Jugador} from './jugador'
 
 var lienzo;
-const velocidad = 10;
+const velocidad = 15;
 
 //JUGADORES
 var p1;
 var p2;
+var pelota;
 
 //MANEJO DEL TECLADO
 var teclas = [];
@@ -22,9 +23,10 @@ window.onload = () =>{
     //SE CREAN LOS DOS JUGADORES*/
     p1 = new Jugador("blue" , lienzo, 100, 400);
     p2 = new Jugador("red", lienzo, 1100, 400);
-
     p1.Preveer(p2); p2.Preveer(p1);
 
+    //SE GENERA LA PELOTA
+    pelota = new Pelota(lienzo, 600, 400, p1, p2);
 
     /*INICIA UN CICLO .setInterval()*/
     setInterval(Actualizar,20);
@@ -58,6 +60,7 @@ function mover(){
     if (teclas && teclas[39]) {p2.moveX(velocidad); }
     if (teclas && teclas[38]) {p2.moveY(-velocidad);}
     if (teclas && teclas[40]) {p2.moveY(velocidad); }
+    
 
 }
 
@@ -68,10 +71,10 @@ function Actualizar(){
     lienzo.clearRect(0,0,1200,800);/*SE BORRA EL CAMBAS*/
 
     //SE VUELVE A PINTAR CADA JUGADOR*/
+    pelota.pintar();
     p1.pintar();
     p2.pintar();
 }   
-
 
 //OTRA PARTE DEL CODIGO
 class Jugador{
@@ -103,13 +106,16 @@ class Jugador{
         let prueba = this.x + nuevaX;
 
         if(prueba>27 && prueba<1177){
+
             this.x += nuevaX;
+            
         }
     
     }
 
     moveY(nuevaY){
         let prueba = this.y + nuevaY;
+
         if(prueba>27 && prueba < 777){
             this.y += nuevaY;
         }
@@ -124,3 +130,69 @@ class Jugador{
     }
 
 }
+
+class Pelota{
+    
+    //CONSTRUCTOR
+    constructor(lienzo ,posx, posy ,jugador1 ,jugador2){
+        this.lienzo = lienzo;
+        this.dx = 15;
+        this.dy = 15;
+        this.x = posx;
+        this.y = posy;
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+    }
+
+    //PINTAR LA PELOTA EN LIENZO
+    pintar(){
+        
+        this.Disparo();
+
+        this.strokeStyle = "black"; 
+
+        this.lienzo.beginPath();
+        this.lienzo.lineWidth = 3;
+        this.lienzo.arc(this.x,this.y,25,0,Math.PI*2,false);
+        this.lienzo.stroke();
+
+        this.lienzo.closePath();
+
+    }
+
+    Disparo(){
+
+        if(this.Colision(this.jugador1) || this.Colision(this.jugador2) ){
+            this.x += 10;
+        }
+
+    }
+
+    Colision(jugador){
+        let dx = this.x - jugador.getX();
+        let dy = this.y - jugador.getY();
+    
+        let distancia = Math.sqrt((dx * dx) + (dy * dy));
+    
+        if(distancia < 55){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+
+/*function Colision(){
+
+    let dx = p1.getX() - p2.getX();
+    let dy = p1.getY() - p2.getY();
+
+    let distancia = Math.sqrt((dx * dx) + (dy * dy));
+
+    if(distancia < 60){
+        return true;
+    }else{
+        return false;
+    }
+}*/
