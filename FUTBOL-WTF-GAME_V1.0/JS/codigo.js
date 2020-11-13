5c4ae9abec1a68744cee2a388bba331f083bd11a
Andrés Chaparro <66017759+Chappie21@users.noreply.github.com>
2020-11-13 19:06:25 -0400

    VERSION 1.0 final de entrega

--- FUTBOL-WTF-GAME_V1.0/JS/codigo.js
+++ FUTBOL-WTF-GAME_V1.0/JS/codigo.js
@@ -1,54 +1,75 @@
-var lienzo;
-const velocidad = 20;
+
+//DATOS DEL LIENZO
+var width; /*ANCHO DEL CANVAS*/
+var height; /*LARGO DEL CANVAS*/
+var lienzo; /*LIENZO DE REFERENCIA*/
 
 //JUGADORES
 var p1;
 var p2;
 var pelota;
-var width;
-var height;
+const velocidad = 20;/*VELOCIDAD CON QUE SE ACTUALIZA EL MOVIMIENTO DE LOS JUGADORES*/
 
 //MANEJO DEL TECLADO
 var teclas = [];
+
 //MENJO DE SONIDOS
 var golazo = ['./SONIDOS/gol1.mp3', './SONIDOS/gol2.mp3', './SONIDOS/gol3.mp3', './SONIDOS/gol4.mp3'];
 var finpar = ['./SONIDOS/fin_1.mp3','./SONIDOS/fin_2.mp3', './SONIDOS/empate.mp3'];
+
 //MARCADOR DE GOLES
 var puntos = [0,0];
+
 //TEMPORIZADOR
 var temp = new Date(180000);
+var cont = 0; /*CONTADOR AUXILIAR DEL TEMPORIZADOR*/
 
-var cont = 0;
 
+//INICIAR EL JUEGO AL CARGAR LA APLICACION WEB
 window.onload = () =>{
 
-    lienzo = document.getElementById("lienzo").getContext("2d");
-    width = document.getElementById("lienzo").width;
-    height = document.getElementById("lienzo").height;
+    lienzo = document.getElementById("lienzo").getContext("2d");/*TOMA EL CONTEXTO DEL NODO CANVAS*/
+    width = document.getElementById("lienzo").width;/*ANCHO DEL CANVAS*/
+    height = document.getElementById("lienzo").height;/*LARGO DEL CANVAS*/
 
+    /*
+        GENERAR LOS EVENTOS "keydown" Y "Keyup" CON LA FINALIDAD DE MANEJAR
+        CUANDO UNA TECLA ES PRECIOANDA Y CUNADO SE DEJA DE PRESIONAR, REALIZANDO
+        EL MOVIMIENTO DE JUGADOR RECPECTIVO
+    */
     window.onkeydown = presionar;
     window.onkeyup = presionar;
 
 
-    //SE CREAN LOS DOS JUGADORES*/
+    //SE CREAN LOS DOS JUGADORES
     p1 = new Jugador("blue" , lienzo, 100, 440, width, height);
     p2 = new Jugador("red", lienzo, 1500, 440,  width, height);
 
     //SE GENERA LA PELOTA
     pelota = new Pelota(lienzo, 800, 440, p1, p2, width, height);
 
+    //SE CREA UN OBJETO DEL TIPO AUDIO CON LA FINALIAD DE REPRODUCIR MUSICA DE FONDO
     let musica = new Audio();
-
     musica.src = "./SONIDOS/musica.mp3";
-
-    musica.play();
+    musica.play(); /*INICIA LA REPRODUCCION DE MUSICA*/
 
     /*INICIA UN CICLO .setInterval()*/
     id = setInterval(Actualizar,20); /*VARIABLE DECLARADA DE FORMA GLOBAL*/
 }
 
+//GESTION DE TECLAS
 function presionar(evento){
+    /*
+        DEPENDIENDO DEL TIPO DE EVENTO, Y LA TECLA PRESIONADA
+        LA CUAL SERÁ LA PSICION UNICA DE ESA TECLA EN EL ARRAY
+        ESTE LO ASIGNARÁ COMO TRUE O FALSE.
 
+        SI ES TRUE, LA TELCA ESTÁ SIENDO PRESIOANDA, SI ES FALSE SE DEJÓ DE PRESIONAR
+        ("keydown" y "keyup" respectivamente).
+
+        GRACIAS A ESTA GESTION LOS JUGADORES PDORÁN MOVERSE EN 8 DIRECCIONES, DANDO MAYOR
+        JUGABILIDAD.
+    */
     switch(evento.type){
 
         case "keydown":
@@ -59,9 +80,9 @@ function presionar(evento){
             teclas[evento.keyCode] = false;
         break;
     }
-
 }
 
+//ACTUALIZAR LA POSICION DE LOS JUGADORES
 function mover(){
 
     /*WASD JUGADOR 1*/
@@ -79,39 +100,44 @@ function mover(){
 
 }
 
+//ACTUALIZACION DE TODO EL CANVAS
 function Actualizar(){
     
     /*
         SE COMPRUEBA SI EL TIEMPO ES MENOR A 0, EN CASO DE SERLO, TERMINA EL JUEGO
         SI NO, CONTINUA EL PROCESO DE CICLADO, DIBUJADO, MOVIMIENTO Y TIEMPO
     */
+
     if(temp.getMinutes()<=0 && temp.getSeconds()<=0){
         let son = new Audio();
         son.src = './SONIDOS/silbato.mp3';
 
-        clearInterval(id);/*SE TERMINA EL CICLO DE INTERVALOS*/
+        clearInterval(id); /*SE TERMINA EL CICLO DE INTERVALOS*/
 
-        son.play();
-        Final();
+        son.play(); /*SUENA SILBATO ANUNCIANDO EL FINAL DEL PARTIDO*/
+        Final(); /*DIBUJA LA PANTALLA FINAL DEL JEUGO*/
     }else{
+
         if(Gol()){
             pelota.setPosicion(800,440);
             p1.setPosicion(100,440);
             p2.setPosicion(1500,440);
         }
     
-        mover();/*SE ACTUALIZA LA POCISION DE LOS JUGADORES*/
-        lienzo.clearRect(0,0,width,height);/*SE BORRA EL CAMBAS*/
-        Barra_y_Campo();
-        Contador();
-        Temporizador();
-        pelota.pintar();
-        p1.pintar();
+        mover(); /*SE ACTUALIZA LA POCISION DE LOS JUGADORES*/
+        lienzo.clearRect(0,0,width,height); /*SE BORRA EL CAMBAS*/
+        Barra_y_Campo(); /*DIBUJA LA BARRA INFORMATIVA Y EL CAMPO*/
+        Contador(); /*ACTUALIZA EL CONTADOR DE PUNTOS EN CASO DE GOL*/
+        Temporizador(); /*DIBUJA EL TEMPROIZADOR*/
+        pelota.pintar(); /*PINTA LA  PELOTA EN SU PCISION RESPECTIVA*/
+
+        //PINTA A SUS JUGADORES EN SU POCISION RESPECTIVA
+        p1.pintar(); 
         p2.pintar();
     }
-
 }   
 
+//DIBUJA EL CAMPO EN CADA ACTUALIZACION
 function Barra_y_Campo(){
 
     lienzo.beginPath();
@@ -123,7 +149,6 @@ function Barra_y_Campo(){
     //CESPED
     lienzo.fillStyle = "rgb(102,180,0)";
     let v = 100;
-
     for(let i=120; i<1600; i+=250){
         lienzo.fillRect(i,20,v,860);
     }
@@ -132,11 +157,13 @@ function Barra_y_Campo(){
     lienzo.strokeStyle = "white";
     lienzo.strokeRect(20,20,width-40,860);
 
+    //MITAD DEL CAMPO
     lienzo.moveTo(800,880);
     lienzo.lineTo(800,20);
     lienzo.stroke();
     lienzo.closePath();
 
+    //LADOS DE CADA JUGADOR
     lienzo.beginPath();
     lienzo.arc(800,440,150,0,2*Math.PI,false);
     lienzo.stroke();
@@ -156,16 +183,16 @@ function Barra_y_Campo(){
     lienzo.stroke();
     lienzo.closePath();
 
-    lienzo.closePath();
+    lienzo.closePath();/*FIN CONPLETO DEL DIBUJADO CANCHA Y BARRA*/
 }
 
-
+//DETECTOR DE GOLES
 function Gol(){
 
     let num = Math.floor(Math.random() * 4);
 
     let sonido = new Audio();
-    sonido.src = golazo[num];
+    sonido.src = golazo[num];/*DEPENDIENDO DEL VALOR, TOMA UNA POSICION DEL ARRAY, EL CUAL ES LA UBICACION DE UN SONIDO*/
 
     if(pelota.getX()<30 && (pelota.getY()>305 && pelota.getY()<605)){
         sonido.play(); puntos[1]++;
@@ -175,14 +202,13 @@ function Gol(){
         return true;
     }
 
-    return false;
+    return false;/*EN CASO DE NO DETECTAR UN GOL, RETORNA FALSE*/
 }
 
+//DIBUJA EL CONTADOR DE PUNTOS
 function Contador(){
     lienzo.font = "bold 65px Verdana";
-
     lienzo.beginPath();
-
     lienzo.fillText(puntos[0], 145,975);
     lienzo.fillText(puntos[1], 1400,975);
 }
@@ -190,16 +216,16 @@ function Contador(){
 //TEMPORIZADOR
 function Temporizador(){
 
+    //CADA VEZ QUE EL CONTADOR AUXILIAR LLEGUE A 50, EL TIEMPO DISMINUYE UN SEGUNDO
     if(cont == 50){
-        temp.setSeconds(temp.getSeconds() - 1);
-        cont = 0;
+        temp.setSeconds(temp.getSeconds() - 1);/*ESTABLECE EL TIEMPO OBTENIDO -1, DISMINUYENDO ESTE POCO A POCO*/
+        cont = 0;/*REINICIA EL CONTADOR AUXILIAR*/
     }else{
-        cont++;
+        cont++;/*AUMENTA EL CONTADOR AUXILIAR*/
     }
 
     lienzo.beginPath();
-    console.log("si");
-    lienzo.fillText(temp.getMinutes()+":"+temp.getSeconds(), 730,975);
+    lienzo.fillText(temp.getMinutes()+":"+temp.getSeconds(), 730,975);/*MUESTRA EL TIEMPO EN PANTALLA*/
 }
 
 //PANTALLA FINAL
--- FUTBOL-WTF-GAME_V1.0/JS/jugador.js
+++ FUTBOL-WTF-GAME_V1.0/JS/jugador.js
@@ -4,24 +4,30 @@ class Jugador{
 
     //CONSTRUCTOR
     constructor(color, lienzo, inx, iny, width, height){
-        this.color = color;
-        this.lienzo = lienzo 
+
+        this.color = color; /*COLOR DEL JUDAOR*/
+        this.lienzo = lienzo  /*LIENZO DE REFERENCIA*/
+
+         //DIMENSIONES DEL LIENZO
         this.width = width;
         this.height = height;
+
+        //POCISION INICIAL
         this.x = inx;
         this.y = iny;
     }
 
-    Preveer(jugador){
-        this.jugador2 = jugador;
-    }
-
+    //PINTA AL JUGADOR EN EL LIENZO DE REFERENCIA
     pintar(){
+
         this.lienzo.beginPath();/*INICIA EL TRAZADO*/
+
+        //DIBUJADO DEL CUERPO
         this.lienzo.fillStyle = this.color;
         this.lienzo.arc(this.x,this.y,30,0,Math.PI*2,false);
         this.lienzo.fill();
 
+        //DIBUJADO DEL CONTORNO
         this.lienzo.lineWidth = 5;
         this.lienzo.stroke();
         this.lienzo.closePath();
@@ -30,29 +36,32 @@ class Jugador{
     moveX(nuevaX){
         let prueba = this.x + nuevaX;
 
+          //COMPRUEBA SI LA POCISION "X" MÁS LA NUEVA ES POSINLE DE REALIZAR
         if(prueba>27 && prueba<this.width-27){
-
             this.x += nuevaX;
-            
         }
     
     }
     moveY(nuevaY){
         let prueba = this.y + nuevaY;
 
+        //COMPRUEBA SI LA POCISION "Y" MÁS LA NUEVA ES POSINLE DE REALIZAR
         if(prueba>27 && prueba < this.height-127){
             this.y += nuevaY;
         }
     }
-
+    
+    //RETORNA LA POCISION X
     getX(){
         return this.x;
     }
 
+    //RETORNA LA POCISION Y
     getY(){
         return this.y;
     }
 
+    //ESTABLECER UNA POCISION
     setPosicion(x,y){
         this.x=x;
         this.y=y;
